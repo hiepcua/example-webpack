86f4fad778cccbc45eaa7f05b165681bee2e70a6
HiepTV <35910113+ffse1701003@users.noreply.github.com>
2019-11-29 18:03:24 +0700

    nothing

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
+import printMe from './print.js';
 
 function component() {
 	const element = document.createElement('div');
+	const btn = document.createElement('button');
 
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-	element.classList.add('hello');
+	btn.innerHTML = 'Click me and check the console!';
+	btn.onclick= printMe;
 
-	// Add the image to our existing div.
-	const myIcon = new Image();
-	myIcon.src = Icon;
-	element.appendChild(myIcon);
-
+	element.appendChild(btn);
 	return element;
 }
 
